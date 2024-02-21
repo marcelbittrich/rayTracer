e9f288ad48aadd449330b35d913173fac1858af4
Marcel Bittrich <marcel.bittrich.dev@gmail.com>
2024-02-21 14:52:46 +0100

    first image

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -1,8 +1,42 @@
 #include "iostream"
 
-int main() 
+#include "color.h"
+#include "vec3.h"
+
+#include "memory"
+
+int main()
 {
-	std::cout << "Hello World" << std::endl;
+	const double aspectRatio = 16.0 / 9.0;
+	const int imageWidth = 100;
+
+	int imageHeight = (int)(imageWidth / aspectRatio);
+	imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+
+	const double viewportHeight = 2.0;
+	const double viewportWidth = viewportHeight * ((double)(imageWidth) / imageHeight);
+
+	// TODO: work with imageBuffer
+	// std::unique_ptr<int[]> imageBuffer = std::make_unique<int[]>(imageHeight * imageWidth * 3);
+
+	std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n" << std::endl;
+
+	for (int j = 0; j < imageHeight; j++)
+	{
+		std::clog << "\rScanlines remaning: " << (imageHeight - j) << ' ' << std::flush;
+		for (int i = 0; i < imageWidth; i++)
+		{
+			double r = (double)i / (imageWidth - 1);
+			double g = (double)j / (imageHeight - 1);
+			double b = 0.0;
+
+			color pixelColor = color(r, g, b);
+		
+			write_color(std::cout, pixelColor);
+		}
+	}
+
+	std::clog << "\rDone.                      \n";
 
 	return 0;
 }
---
+++ RayTracingInOneWeekend/ray.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "vec3.h"
+
+class Ray
+{
+public:
+	Ray() {}
+
+	Ray(const point3& origin, const vec3& direction)
+		: m_origin(m_origin), m_direction(direction) {}
+
+	point3 origin() const { return m_origin; }
+	vec3 direction() const { return m_direction; }
+
+	point3 at(double t) const {
+		return m_origin + m_direction * t; 
+	}
+
+private:
+	vec3 m_origin;
+	vec3 m_direction;
+};
